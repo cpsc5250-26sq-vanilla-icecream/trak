9fa9f851532aded95db5dee45f3c89f6d88c5209
Heidrafps <kairuchanvlogs@gmail.com>
2026-04-28 00:28:32 -0700

    Added health_repository.dart to ask and get Steps from Android Health Connect application.

---
+++ lib/repository/health_repository.dart
@@ -0,0 +1,22 @@
+import 'package:health/health.dart';
+
+class HealthRepository {
+  final Health _health;
+  HealthRepository(this._health);
+
+  // Get permission from Health Connect
+  // This must be called before getting data!
+  Future<void> requestPermission() async {
+    await _health.requestAuthorization([HealthDataType.STEPS]);
+  }
+
+  // Get the step count from Health Connect
+  Future<int> getTodaySteps() async {
+    final now = DateTime.now();
+    final start = DateTime(now.year, now.month, now.day);
+
+    final steps = await _health.getTotalStepsInInterval(start, now);
+
+    return steps ?? 0;
+  }
+}
