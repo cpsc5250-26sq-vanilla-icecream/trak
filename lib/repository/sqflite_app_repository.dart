5f00173cb282af121133d72f4b04537e76422602
Heidrafps <kairuchanvlogs@gmail.com>
2026-04-28 17:52:59 -0700

    Implemented Steps implementation in sqflite_app_repository.dart and how it interacts with app_database.dart

--- lib/repository/sqflite_app_repository.dart
+++ lib/repository/sqflite_app_repository.dart
@@ -1,12 +1,32 @@
+import 'dart:async';
 import 'package:trak/models/inventory_item.dart';
 import 'package:trak/models/leaderboard_entry.dart';
 import 'package:trak/models/use_item_result.dart';
 import 'package:trak/models/user_profile.dart';
 import 'package:trak/repository/app_repository.dart';
 import '../database/app_database.dart';
+import 'health_repository.dart';
 
 class SqfliteAppRepository implements AppRepository {
   final AppDatabase db = AppDatabase.instance;
+  final HealthRepository health = HealthRepository();
+
+  // Todo: Get user AUTH
+  final String currentUserId = "";
+  final _stepsController = StreamController<int>.broadcast();
+
+  // Get Today's Date
+  String _today() => DateTime.now().toIso8601String().split('T').first;
+
+  // Update Widgets Through Stream Controller
+  Future<void> _notify() async {
+    final steps = await db.getCurrentSteps(
+      userId: currentUserId,
+      date: _today(),
+    );
+    _stepsController.add(steps ?? 0);
+  }
+
   @override
   Future<void> addFriend(String targetUserId) {
     // TODO: implement addFriend
@@ -20,9 +40,17 @@ class SqfliteAppRepository implements AppRepository {
   }
 
   @override
-  Future<void> putSteps(int stepCount) {
-    // TODO: implement putSteps
-    throw UnimplementedError();
+  Future<void> putSteps(int stepCount) async {
+    await health.requestPermission();
+    final steps = await health.getTodaySteps();
+
+    await db.upsertSteps(
+      userId: currentUserId,
+      date: _today(),
+      stepCount: steps,
+    );
+
+    await _notify();
   }
 
   @override
@@ -51,7 +79,7 @@ class SqfliteAppRepository implements AppRepository {
 
   @override
   Stream<int> watchStepCount() {
-    // TODO: implement watchStepCount
-    throw UnimplementedError();
+    _notify();
+    return _stepsController.stream;
   }
 }
